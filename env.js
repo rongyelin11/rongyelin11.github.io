7b10aaabe1c9ba45709b0b2c00aeaef86346ebd3
rongyelin11 <rong.ye.lin2002@gmail.com>
2026-04-25 15:28:06 +0200

    portfolio completo

---
+++ env.js
@@ -0,0 +1,26 @@
+// Auto-generated environment config for self-hosted Dora viewer.
+(function() {
+  var host = location.hostname;
+  var port = location.port;
+  var localHost = port ? host + ":" + port : host;
+
+  window.__DORA_ENV = {
+    RELEASE_VERSION: "0.0.0",
+    TARGET: "online",
+    HOME_HOST: localHost,
+    RUN_HOST: localHost,
+    PREVIEW_HOST: localHost,
+    SWIMLANE_HOST: "",
+    API_HOST: localHost,
+    NEO_API_HOST: localHost,
+    CDN_NEO_API_HOST: localHost,
+    STATIC_ASSETS_HOST: localHost,
+    USER_ASSETS_HOST: localHost,
+    CDN_IMGS_HOST: localHost,
+    CDN_PUBLIC_DOC_HOST: localHost,
+    REALTIME_HOST: ""
+  };
+
+  window.__DORA_ASSET_PREFIX = "";
+  window.__DORA_ASSET_MANIFEST = {};
+})();
